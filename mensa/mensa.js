893436ccd14acf25d88516d86d8e4b9d3c012cf5
Sebastian <sebastian.ampuerom@gmail.com>
2017-06-24 23:05:51 +0200

    Init commit

---
+++ mensa/mensa.js
@@ -0,0 +1,31 @@
+const request = require('request');
+
+var getMensaOptions = () =>{
+  return new Promise((resolve,reject)=>{
+    request({
+      url : 'https://icampus.th-wildau.de/services.php?service=Canteen',
+      proxy : 'http://proxy.th-wildau.de:8080',
+      json:true
+    },(error,response,body)=>{
+      if(error){
+        reject('Unable to connect');
+      }else{
+        resolve({
+          today : body.response.today.options,
+          nextDates : body.response.future_dates,
+          mensaClosedMsg : body.response.message
+        });
+      }
+    })
+  });
+}
+
+getMensaOptions().then((result)=>{
+  console.log(result.mensaClosedMsg!=null);
+},(error)=>{
+  console.log(error);
+})
+
+module.exports = {
+  getMensaOptions
+}
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const hbs = require('hbs');
+const moment = require('moment');
+const mensa = require('./mensa/mensa.js')
+var app = express();
+const port = process.env.port || 3000;
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('getDate',()=>{
+  return moment().format('LL');
+});
+hbs.registerHelper('formatDate',(date)=>{
+  moment.locale('de');
+  var theDate = moment(date).format('dddd');
+  return theDate;
+})
+
+
+app.get('/',(req,res)=>{
+  mensa.getMensaOptions().then((result)=>{
+    if(result.today.length === 0){
+      res.render('home.hbs',{
+        closed : result.mensaClosedMsg.de_DE
+      });
+    }else{
+      res.render('home.hbs',{
+        options : result.today
+      })
+    }
+  },(error)=>{
+    res.send('error');
+  })
+});
+app.get('/future',(req,res)=>{
+  mensa.getMensaOptions().then((result)=>{
+    res.render('future_dates.hbs',{
+      days : result.nextDates
+    })
+  })
+});
+
+app.listen(port, ()=>{
+  console.log('Listening on port',port);
+});
